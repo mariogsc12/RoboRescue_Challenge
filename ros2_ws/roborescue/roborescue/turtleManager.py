85e7cf3ba24f0c07931ec30ef6e2597450381eb0
Mario Gomez <m.gomez03016@gmail.com>
2025-05-19 22:59:30 +0200

    Add B and O letters (draw ROBO)

--- ros2_ws/roborescue/roborescue/turtleManager.py
+++ ros2_ws/roborescue/roborescue/turtleManager.py
@@ -5,7 +5,7 @@ from rclpy.node import Node
 from rclpy.action import ActionClient
 from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
-from turtlesim.srv import SetPen, TeleportAbsolute, TeleportRelative
+from turtlesim.srv import SetPen, TeleportAbsolute, TeleportRelative, Spawn
 from functools import partial
 from roborescue.action import GoTo
 from rclpy.executors import ExternalShutdownException
@@ -19,6 +19,18 @@ class DrawLetters:
         self.length = length
         self.width = width
 
+    def letter_manager(self, letter_, origin_x, origin_y):
+        letter = letter_.upper()
+        if letter == "R":
+            return self.draw_R(origin_x, origin_y)
+        elif letter == "O":
+            return self.draw_O(origin_x, origin_y)
+        elif letter == "B":
+            return self.draw_B(origin_x, origin_y)
+        else:
+            self.get_logger().error(f"The letter {letter} is not configured.")
+            return
+
     def draw_R(self, origin_x=5.54, origin_y=5.54):
         """ Returns a list of points (x,y) to draw the letter R"""
         l = self.length
@@ -26,27 +38,58 @@ class DrawLetters:
         x0, y0 = origin_x, origin_y
 
         points = [
-            (x0, y0),               # inicio
-            (x0, y0 + l),           # linea vertical hacia arriba de longitud altura de letra
-            (x0 + w, y0 + l),       # linea horizontal hacia derecha de longitud ancho de letra
-            (x0 + w, y0 + l / 2),   # Bajada de longitud mitad de alto de letra
-            (x0 + w/2, y0 + l / 2), # Horizontal hacia izquierda hasta la mitad del valor de la longitud del ancho
-            (x0 + w, y0),           # diagonal hasta el punto con Y=0 y X=ancho
-            (x0 + w / 2, y0),       # horizontal hasta la mitad del ancho
-            (x0 + w/3, y0 + l / 2), # Diagonal hacia la mitad de la altura de la letra
-            (x0 + w/3, y0),         # Vertical hacia abajo hasta el punto con Y=0 y X=ancho/3
-            (x0, y0)                # Horizontal hacia la izquierda hasta el punto de inicio
+            (x0, y0),               # start point
+            (x0, y0 + l),           # vertical line upward by letter height
+            (x0 + w, y0 + l),       # horizontal line right by letter width
+            (x0 + w, y0 + l / 2),   # downward line by half the letter height
+            (x0 + w/2, y0 + l / 2), # horizontal line left to half the letter width
+            (x0 + w, y0),           # diagonal to the point at y=0 and x=width
+            (x0 + w / 2, y0),       # horizontal line to half the width
+            (x0 + w/3, y0 + l / 2), # diagonal to halfway up the letter height
+            (x0 + w/3, y0),         # vertical line down to the point at y=0 and x=width/3
+            (x0, y0)                # horizontal line left back to the start point
         ]
+
+        return points
+
+    def draw_O(self, origin_x=5.54, origin_y=5.54):
+        """ Returns a list of points (x,y) to draw the letter O"""
+        l = self.length
+        w = self.width
+        x0, y0 = origin_x, origin_y
+
+        points = [
+            (x0, y0),               # start point
+            (x0, y0 + l),           # vertical line upward by letter height
+            (x0 + w, y0 + l),       # horizontal line right by letter width
+            (x0 + w, y0),           # vertical line downward by letter height
+            (x0, y0)                # horizontal line left back to the start point
+        ]
+
+        return points
+
+    def draw_B(self, origin_x=5.54, origin_y=5.54):
+        """ Returns a list of points (x,y) to draw the letter B"""
+        l = self.length
+        w = self.width
+        x0, y0 = origin_x, origin_y
+
+        points = [
+            (x0, y0),               # start point
+            (x0, y0 + l),           # vertical line upward by letter height
+            (x0 + w, y0 + l),       # horizontal line right by letter width
+            (x0 + w/2, y0 + l/2),   # diagonal to the center point
+            (x0 + w, y0),           # diagonal to the right down point
+            (x0, y0)                # horizontal line left back to the start point
+        ]
+
         return points
 
 class TurtleManager(Node):
     def __init__(self):
         super().__init__("turtle_manager")
-        self.pose_sub = self.create_subscription(Pose, '/turtle1/pose', self.poseCallback, 10)
-        #self.cmd_vel_pub = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
 
         self._action_client = ActionClient(self, GoTo, 'GoTo')
-        self.current_pose = Pose()
 
         self.get_config_parameters()
 
@@ -56,39 +99,50 @@ class TurtleManager(Node):
         self.trajectory_planner()
 
     def get_config_parameters(self):
-        self.declare_parameter('turtlesim_screen_max', 11.0) 
-        self.declare_parameter('turtlesim_screen_min', 0.0) 
+        self.declare_parameter('word', "RO") 
         self.declare_parameter('screen_color_limit', 5.0) 
         self.declare_parameter('letter_width', 1.0) 
         self.declare_parameter('letter_height', 3.0) 
         self.declare_parameter('screen_color', [0,0,255]) 
         self.declare_parameter('line_default_color', [255,255,255]) 
         self.declare_parameter('line_width', 3) 
+        self.declare_parameter('letter_offset', [0.25,0.0]) 
 
-        self.screen_max = self.get_parameter('turtlesim_screen_max').get_parameter_value().double_value
-        self.screen_min = self.get_parameter('turtlesim_screen_min').get_parameter_value().double_value
+        self.word = self.get_parameter('word').get_parameter_value().string_value
         self.screen_color_limit = self.get_parameter('screen_color_limit').get_parameter_value().double_value
         self.letter_width = self.get_parameter('letter_width').get_parameter_value().double_value
         self.letter_height = self.get_parameter('letter_height').get_parameter_value().double_value
         self.screen_color = self.get_parameter('screen_color').get_parameter_value().integer_array_value
         self.line_default_color = self.get_parameter('line_default_color').get_parameter_value().integer_array_value
         self.line_width = self.get_parameter('line_width').get_parameter_value().integer_value
+        letter_offset = self.get_parameter('letter_offset').get_parameter_value().double_array_value
+
+        self.letter_offset_x = letter_offset[0]
+        self.letter_offset_y = letter_offset[1]
 
     
     def trajectory_planner(self):
         """ Wrapper function to manage the turtle trajectory"""
         
         initial_x = 1.0
-        initial_y = 1.0
+        initial_y = 5.0
 
-        self.teleport_absoulte_service(initial_x,initial_y,0.0)
-        for waypoint in self.letter_info.draw_R(initial_x,initial_y):
-            goal_future = self.send_goal(waypoint[0], waypoint[1], 0.0)
-            rclpy.spin_until_future_complete(self, goal_future)
+        for letter in self.word:
+            self.get_logger().info(f'--- Starting to draw {letter} at {initial_x,initial_y} ---')
+            self.teleport_absoulte_service(initial_x,initial_y,0.0)
+            waypoints = self.letter_info.letter_manager(letter,initial_x,initial_y)
 
+            # Loop to move the turtle to the waypoints of the letter (skip the first one to keep the "hide" line)
+            for wp in waypoints[1:]:
+                goal_future = self.send_goal(wp[0], wp[1], 0.0)
+                rclpy.spin_until_future_complete(self, goal_future)
+
+            initial_x = initial_x + self.letter_width + self.letter_offset_x
+            initial_y = initial_y + self.letter_offset_y
+
+    # -------- ACTIONS -------
     def send_goal(self, x, y, theta):
         # Wait server
-        self.get_logger().info('Waiting for action server...')
         self._action_client.wait_for_server()
 
         self.get_logger().info(f"Sending goal: {x, y, theta}")
@@ -134,9 +188,22 @@ class TurtleManager(Node):
     def feedback_callback(self, feedback_msg):
         feedback = feedback_msg.feedback
         #self.get_logger().info(f'Current position: x={feedback.x:.2f}, y={feedback.y:.2f}')
-        
-    def poseCallback(self, pose_msg:Pose):
-        self.current_pose = pose_msg
+
+    # ---------- SERVICES ---------
+    def spawn_turtle_service(self, x, y, theta, name):
+        client = self.create_client(Spawn, "/turtle1/spawn")
+        while not client.wait_for_service(1.0):
+            self.get_logger().info("Waiting for service spawn...")
+
+        req = Spawn.Request()
+        req.x = x
+        req.y = y
+        req.theta = theta
+        req.name = name
+        future = self.cli.call_async(req)
+        rclpy.spin_until_future_complete(self, future)
+        if future.result():
+            self.get_logger().info(f"Spawned {future.result().name} at ({x},{y})")
 
     def pen_service(self, r, g, b, width, off):
         client = self.create_client(SetPen, "/turtle1/set_pen")
